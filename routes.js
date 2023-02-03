913aa97f3ea9953f353fb8a5a9e1eb0308308a2a
vahushty <juarinio228@gmail.com>
2023-02-03 14:33:07 +0400

    Yahay bilya

---
+++ routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const host = "localhost";
+const port = 8000;
+
+let i = "Off";
+
+app.get("/on", function (req, res) {
+  res.writeHead(200);
+  i = "On";
+  res.end(`Power ${i}`);
+});
+app.get("/off", function (req, res) {
+  res.writeHead(200);
+  i = "Off";
+  res.end(`Power ${i}`);
+});
+app.get("/status", function (req, res) {
+  res.writeHead(200);
+  res.end(`Power status = ${i}`);
+});
+app.listen(port, host, function () {
+  console.log(`Server is running on http://${host}:${port}`);
+});
